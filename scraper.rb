8daaaee24996552972d77b297587eadd331a7035
Ukaza Perdana <hello@ukazap.space>
2018-10-01 11:45:04 +0800

    fix nil error

--- scraper.rb
+++ scraper.rb
@@ -21,14 +21,15 @@ loop do
 
     features = {
       "name" => breadcrumb_link[3].content,
-      "brand" => breadcrumb_link[2].content,
-      "automatic_transmission" => features['Jenis Transmisi'].strip != 'Manual'
+      "brand" => breadcrumb_link[2].content
     }
 
     detail_page.search('.feature-list td').each_slice(2) do |feature|
       name, value = feature
       features[name.content.downcase.gsub(" ", "_")] = value.content
     end
+
+    features["automatic_transmission"] = features['Jenis Transmisi'].strip != 'Manual'
 
     ScraperWiki.save_sqlite(["name"], features)
   end
